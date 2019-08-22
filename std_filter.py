b999116e75542ac741c1bfc8926b2b8e50909f32
mvt-lsf <31967046+mvt-lsf@users.noreply.github.com>
2019-08-21 23:58:31 -0300

    Update std_filter.py
    falta cambios en .hdr

--- std_filter.py
+++ std_filter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import glob,os,time
 
 def mv_avg_std(matrix,window,window_z):
 
@@ -19,27 +20,34 @@ def mv_avg_std(matrix,window,window_z):
             suma_actual_z=suma_actual_z-res[:,j-1]+res[:,j+window-1]
             res[:,j]=suma_actual_z/window_z    
 
-    return np.std(res[:,:matrix.shape[1]-window_z+1],axis=0)/np.mean(matrix[:,:matrix.shape[1]-window_z+1],axis=0)
+    return np.std(res[:,:matrix.shape[1]-window_z+1],axis=0), np.mean(matrix[:,:matrix.shape[1]-window_z+1],axis=0)
 
 def std_filter(raw_m,shotPorChunk=1000,window_time=40,window_z=1):
     n=raw_m.shape[0]/shotPorChunk
     wf=np.zeros((n,raw_m.shape[1]-window_z+1))
+    wf_avg=np.zeros((n,raw_m.shape[1]-window_z+1))
     for i in xrange(n):
         esteChunk=raw_m[i*shotPorChunk:(i+1)*shotPorChunk,:] 
-        esteChunkSTD=mv_avg_std(esteChunk,window_time,window_z)
+        esteChunkSTD,esteChunkAVG=mv_avg_std(esteChunk,window_time,window_z)
         wf[i,:]=esteChunkSTD
-        print i,n
-    return wf
+        wf_avg[i,:]=esteChunkAVG
+    return wf,wf_avg
+
+def moving_average(a, n=300) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
 
 #%%
-lineas=1000
-shotPorChunk=1000
-bins_raw=2000
-archivo=
-shots=lineas*shotPorChunk
-data=np.fromfile(archivo,dtype=np.int16,count=shots*bins_raw).reshape(-1,bins_raw)
-#%%
-wf=std_filter(data)
-#%%
-plt.imshow(wf,aspect='auto',cmap='jet',vmax=0.2)
+for archivo in glob.glob('*.bin'):
+    bins_raw=2000
+    print "filtrando ",archivo
+    st=time.time()
+    data=np.fromfile(archivo,dtype=np.int16).reshape(-1,bins_raw)
+    wf,wf_avg=std_filter(data)
+    moving_average(wf).astype(np.float32).tofile(archivo[:-3]+'std')
+    print "creado ", archivo[:-3]+'std'
+    moving_average(wf_avg).astype(np.float32).tofile(archivo[:-3]+'avg')
+    print "creado ", archivo[:-3]+'avg'
+    print "tarda ", (time.time()-st), "seg por archivo\n"
